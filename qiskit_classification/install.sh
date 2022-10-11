f01e33231431cc322f597f756da353f2d4065a79
David Berga <david.berga@eurecat.org>
2022-10-11 15:10:39 +0200

    updated qiskit install dependency versions

--- qiskit_classification/install.sh
+++ qiskit_classification/install.sh
@@ -1,4 +1,4 @@
-!pip install qiskit qiskit-textbook qiskit-machine-learning qiskit-aqua qiskit-terra qiskit-aer qiskit-ignis qiskit-ibmq-provider
-!pip install numexpr numpy
-!pip install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu112/torch_nightly.html
-
+pip install qiskit==0.36.1 qiskit-machine-learning==0.4.0 qiskit-aqua==0.9.5 qiskit-terra==0.20.1 qiskit-aer==0.10.4 qiskit-ignis==0.7.0 qiskit-ibmq-provider==0.19.1
+pip install git+https://github.com/qiskit-community/qiskit-textbook.git#subdirectory=qiskit-textbook-src
+pip install numexpr numpy
+pip install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu112/torch_nightly.html
